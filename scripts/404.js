5358a10a8e944ace24efaf4dbd4aecc3787a6fc5
Stiven Ding <12206788+stiven-ding@users.noreply.github.com>
2021-02-16 10:18:42 +0800

    404

--- scripts/404.js
+++ scripts/404.js
@@ -2,8 +2,6 @@ var entry =  window.location.pathname.split('/')[1];
 var course_code = window.location.pathname.split('/')[2];
 var prof_name = window.location.pathname.split('/')[3];
 setTimeout(function () {
-    if(entry.indexOf('html')>-1)
-        window.location.href = '/';
-    else
+    if(entry.indexOf('html')>0 || !course_code == undefined)
         window.location.href = '/'+ entry +'.html?New_code=' + course_code + '&prof_name=' + prof_name;
-}, 1000);
+}, 0);
