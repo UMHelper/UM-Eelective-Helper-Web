3f61ea7e19319d924efadfac815a0b3b7c7c7f19
Stiven Ding <12206788+stiven-ding@users.noreply.github.com>
2021-03-06 11:27:36 +0800

    index layout

--- scripts/index.js
+++ scripts/index.js
@@ -4,19 +4,6 @@ function changeType() {
     document.getElementById("course_input").placeholder = "Eg. " + (document.getElementById("byCrn").checked == true ? "ACCT1000" : "CHAN TAI MAN");
 }
 
-
-
-function getNums(resp_json) {
-    var com = resp_json.faculty_detail;
-
-    document.getElementById("connecting").innerHTML = "DATABASE OF REVIEWS";
-    for (var i in com) {
-        document.getElementById("numsPanel").innerHTML += '<div class="numsItem"><div style="font-weight: bolder;">' + i + '</div><div>' + com[i].comment + '</div></div>'
-    }
-    
-    document.getElementById("progress").style.visibility = "hidden";
-}
-
 function goSearch() {
     document.getElementById("progress").style.visibility = "visible";
     if (document.getElementById("course_input").value.length < 4) {
@@ -35,28 +22,35 @@ function goSearch() {
 }
 
 // init
-search_button.onclick = goSearch;
-document.getElementById("byCrn").onclick = changeType;
-document.getElementById("byIns").onclick = changeType;
-
+/*search_button.onclick = goSearch;
 document
     .getElementById("course_input")
     .setAttribute("onkeypress", "if(event.keyCode==13) {goSearch()}");
 
-document.getElementById("course_input").value = "";
+document.getElementById("course_input").value = "";*/
 
 // get total num
-try {
-    var req = new XMLHttpRequest();
-    req.onreadystatechange = function () {
-        if (req.readyState === XMLHttpRequest.DONE) {
-            var resp_json = JSON.parse(req.responseText);
-            getNums(resp_json);
+$.ajax({
+    url: API_server + "/get_stat/",
+    dataType: "json",
+    success: function (data) {
+        var com = data.faculty_detail;
+        for (var i in data.faculty_detail) {
+            $("#numsPanel").append('<div class="numsItem"><div style="font-weight: bolder;">' + i + '</div><div>' + data.faculty_detail[i].comment + '</div></div>');
         }
-    };
+    },
+    error: function (data) {
+        $("#numsPanel").replaceWith("ERROR");
+    }
+});
 
-    req.open("GET", API_server + "/get_stat/");
-    req.send();
-} catch (e) {
-    myAlert(`Network issue or we're down😇. \n網路問題或者我們的服務中斷\n Error: ${e}.`);
-}
+$.ajax({
+    url: "https://api.github.com/repos/UMHelper/UM-Eelective-Helper-Web/branches/main",
+    dataType: "json",
+    success: function (data) {
+        $('#version').replaceWith('<a href="'+ data.commit.html_url + '">'+ data.commit.commit.author.date);
+    },
+    error: function (data) {
+        $('#version').replaceWith("ERROR");
+    }
+});
