d4b88701593e2d092cccc78da8076754ddcc2362
Stiven Ding <12206788+stiven-ding@users.noreply.github.com>
2021-03-09 18:00:58 +0800

    fixed non-aligned cards on course

--- scripts/404.js
+++
@@ -1,1 +0,0 @@
-console.log('The web subdomain will no longer automaticly redirect to dynamic URL. For local testing, please setup a webserver (nginx or IIS) and set the rewrite rule. Refer to \\web.config');
