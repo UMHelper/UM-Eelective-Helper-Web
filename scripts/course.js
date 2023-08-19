a464d0a79a9d932b89b053eaf07e2f78d2c36e90
Stiven Ding <12206788+stiven-ding@users.noreply.github.com>
2023-08-19 22:18:24 +0800

    change bot detect algo

--- scripts/course.js
+++ scripts/course.js
@@ -19,7 +19,24 @@ $(window).on('scroll', function () {
 */
 
 // crawler detection
-if(/bot|google|baidu|bing|msn|teoma|slurp|yandex/i.test(navigator.userAgent)) {
+const detectRobot = (userAgent) => {
+  const robots = new RegExp([
+    /bot/,/spider/,/crawl/,                            // GENERAL TERMS
+    /APIs-Google/,/AdsBot/,/Googlebot/,                // GOOGLE ROBOTS
+    /mediapartners/,/Google Favicon/,
+    /FeedFetcher/,/Google-Read-Aloud/,
+    /DuplexWeb-Google/,/googleweblight/,
+    /bing/,/yandex/,/baidu/,/duckduck/,/yahoo/,        // OTHER ENGINES
+    /ecosia/,/ia_archiver/,
+    /facebook/,/instagram/,/pinterest/,/reddit/,       // SOCIAL MEDIA
+    /slack/,/twitter/,/whatsapp/,/youtube/,
+    /semrush/,                                         // OTHER
+  ].map((r) => r.source).join("|"),"i");               // BUILD REGEXP + "i" FLAG
+
+  return robots.test(userAgent);
+};
+
+if(detectRobot(navigator.userAgent)) {
   $('#googleBotCourseInfo').css('display', 'block');
 }
 else {
